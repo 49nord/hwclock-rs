fe51bbe27b0c5e0076f17adeb01d6b900b4a39cb
Marc Brinkmann <marc@49nord.de>
2018-02-19 14:54:05 +0100

    Initial impl with conversion to `chrono` `NaiveDateTime`.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,68 @@
+extern crate chrono;
 extern crate libc;
+#[macro_use]
+extern crate nix;
 
 use libc::c_int;
 
-/// Real-time clock time structure
-#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+// ioctls, stolen from linux/rtc.h
+ioctl!(read rtc_rd_time with 'p', 0x09; RtcTime);
+ioctl!(write_ptr rtc_set_time with 'p', 0x0a; RtcTime);
+
+/// Linux `struct rtc_time`
+///
+/// This structure is slightly shorter than other commonly used `struct tm*`.
+/// It is assumed that the Rtc is kept at UTC.
+///
+/// Note that the resolution of the time struct is only seconds.
 #[repr(C)]
-struct rtc_time {
-    /// Seconds part of time
-    tm_sec: c_int,
-    /// Minutes part of time
-    tm_min: c_int,
-    /// Hour part of time
-    tm_hour: c_int,
-    /// Day of the month
-    tm_mday: c_int,
-    /// Month, starting at Jan=1
-    tm_mon: c_int,
-    /// Year
-    tm_year: c_int,
+#[derive(Debug, Copy, Clone, Default)]
+pub struct RtcTime {
+    /// Seconds
+    pub tm_sec: c_int,
+    /// Minutes
+    pub tm_min: c_int,
+    /// Hours
+    pub tm_hour: c_int,
+    /// Day of the month, first day is 0
+    pub tm_mday: c_int,
+    /// Month of the year, first month (January) is 0
+    pub tm_mon: c_int,
+    /// Year, starting at 118
+    pub tm_year: c_int,
     /// unused
-    tm_wday: c_int,
+    pub tm_wday: c_int,
     /// unused
-    tm_yday: c_int,
+    pub tm_yday: c_int,
     /// unused
-    tm_isdst: c_int,
+    pub tm_isdst: c_int,
+}
+
+impl From<RtcTime> for chrono::NaiveDateTime {
+    fn from(rtc: RtcTime) -> chrono::NaiveDateTime {
+        let d = chrono::NaiveDate::from_ymd(
+            (rtc.tm_year + 1900) as i32,
+            (rtc.tm_mon + 1) as u32,
+            (rtc.tm_mday + 1) as u32,
+        );
+        let t =
+            chrono::NaiveTime::from_hms(rtc.tm_hour as u32, rtc.tm_min as u32, rtc.tm_sec as u32);
+        chrono::NaiveDateTime::new(d, t)
+    }
+}
+
+#[test]
+fn bindgen_test_layout_rtc_time() {
+    assert_eq!(
+        ::std::mem::size_of::<RtcTime>(),
+        36usize,
+        concat!("Size of: ", stringify!(RtcTime))
+    );
+    assert_eq!(
+        ::std::mem::align_of::<RtcTime>(),
+        4usize,
+        concat!("Alignment of ", stringify!(RtcTime))
+    );
 }
 
 fn main() {
