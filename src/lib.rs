29534753b86e7cec8b7abdfa22396ff41a9b556a
Marc Brinkmann <marc@49nord.de>
2018-02-19 16:04:57 +0100

    Turned binary into library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+//! Hardware clock handling for linux
+//!
+//! ```rust,no_run
+//! extern crate chrono;
+//! extern crate hwclock;
+//!
+//! fn main() {
+//!     use hwclock::HwClockDev;
+//!
+//!     let rtc = HwClockDev::open("/dev/rtc0").expect("could not open rtc clock");
+//!
+//!     println!("{:?}", rtc);
+//!
+//!     let time = rtc.get_time().expect("could not read rtc clock");
+//!     println!("{:?}", time);
+//!
+//!     println!("Setting clock ahead 30 seconds");
+//!     let mut ct: chrono::NaiveDateTime = time.into();
+//!     ct += chrono::Duration::seconds(30);
+//!
+//!     // convert back to RtcTime and set it
+//!     let ntime = ct.into();
+//!     rtc.set_time(&ntime).expect("could not set rtc clock");
+//!
+//!     println!("Rereading...");
+//!     let time2 = rtc.get_time().expect("could not read rtc clock");
+//!
+//!     println!("{:?}", time2);
+//! }
+//! ```
+
 extern crate chrono;
 extern crate libc;
 #[macro_use]
@@ -159,8 +190,4 @@ impl HwClockDev {
 
         Ok(())
     }
-}
-
-fn main() {
-    let rtc_dev = "/dev/rtc1";
 }
